6a23a1ea7f7250c17317d83b676646e88df78984
Renan Huanca <rhuanca@gmail.com>
2022-08-11 20:20:27 -0400

    update

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -2,5 +2,10 @@ import streamlit as st
 
 st.write("# 10 Cool Beginner Python Tricks That Will Make Your Life Easier")
 
+st.write("""
+    ## 1. Walrus operator
+    The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
+""")
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 100, 20)
