097cc0d9060a88b76d3be427da20478bf72ede3a
Renan Huanca <rhuanca@gmail.com>
2022-08-11 20:19:07 -0400

    update

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 
-st.write("HELLO FROM THE BOOTCAMP")
+st.write("# 10 Cool Beginner Python Tricks That Will Make Your Life Easier")
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 100, 20)
