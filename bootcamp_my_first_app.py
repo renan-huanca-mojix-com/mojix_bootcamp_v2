b7ad410ce78dede1e60fd969844fb8ec6edcbc0d
Renan Huanca <rhuanca@gmail.com>
2022-08-11 20:17:16 -0400

    second commit

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.write("HELLO FROM THE BOOTCAMP")
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    total_points = st.slider("Number of points in spiral", 1, 100, 20)
