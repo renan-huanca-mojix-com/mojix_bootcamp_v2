9505d31bc8d5e2b4c61b18cc82732ed2561a5c40
Renan Huanca <rhuanca@gmail.com>
2022-08-11 20:21:25 -0400

    update

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -3,9 +3,21 @@ import streamlit as st
 st.write("# 10 Cool Beginner Python Tricks That Will Make Your Life Easier")
 
 st.write("""
-    ## 1. Walrus operator
-    The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
+## 1. Walrus operator
+The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.
+### Example
+If we want to check and print the length of a list:
+
+Mylist = [1,2,3]
+if(l := len(mylist) > 2)
+print(l)
+     
 """)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 100, 20)
+
+
+
+
+
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 100, 20)
